cec44528d348f7fd43ceecbcac4aef2ce30c5c4f
CLF78 <29682335+CLF78@users.noreply.github.com>
2024-08-22 18:42:07 +0200

    Fix Thwomp Antifreeze crash caused by unwanted compiler optimizations

--- payload/game/obj/ObjDossun.cpp
+++ payload/game/obj/ObjDossun.cpp
@@ -7,8 +7,12 @@
 
 // Prevent Thwomps from crashing the game when they are hit before they land the first time
 // Credits: MrBean35000vr
-kmBranchDefCpp(0x80760A88, 0x80760A98, float, AnmHolder* anm) {
+kmCallDefCpp(0x80760A88, float, AnmHolder* anm) {
 
-	// Get animation frame count, otherwise a dummy value
-	return (anm != nullptr) ? anm->GetFrameCount() : 0.0f;
+    // Get animation frame count, otherwise a dummy value
+    return (anm != nullptr) ? anm->GetFrameCount() : 0.0f;
 }
+
+// Glue code
+// kmBranchDefCpp cannot be used here due to compiler optimizations
+kmBranch(0x80760A8C, 0x80760A98);
